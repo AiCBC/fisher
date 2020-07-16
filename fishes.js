256df3031ccb29146aa73acc89fb41620fffbd42
xiabo2 <58075128+xiabo2@users.noreply.github.com>
2020-07-16 21:41:43 +0800

    Add files via upload

---
+++ fishes.js
@@ -0,0 +1,22 @@
+// 页脚鱼
+fish();
+function fish() {
+    return (
+      $("#footer-wrap").css({
+        position: "absolute",
+        "text-align": "center",
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+        "z-index": auto,
+      }),
+      $("footer").append(
+        '<div class="container" id="jsi-flying-fish-container"></div>'
+      ),
+      $("body").append(
+        '<script src="https://cdn.jsdelivr.net/gh/fudalijunyi/cdn/lovelijunyi/js/fish.js"></script>'
+      ),
+      this
+    );
+  }
